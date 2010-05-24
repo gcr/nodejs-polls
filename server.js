f78acc6d8910396a73e6433c25497a2475292f9c
Michael W <hotdog003@gmail.com>
2010-05-24 08:47:18 -0600

    added comments

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ var
   PORT        = 8080,
   count=0;
 
+// Some notes. We'll need some sort of very simple RPC system.
+// Server:
+//   List of prepared questions
+//       "Create new" either makes a question or adds it to the prepared
+//       questions
+//       "Edit" a prepared question
+//       Save it to db or files! Probably files
+//   Then the "Waiting for votes..." screen
+//     Constantly polls for number of votes
+//     Will ask to end the poll
+//   Then the "Poll Results" screen.
+//     Buttons to create a new poll
+//
+// Voter:
+//   Just one "open" question at a time.
+//   It will constantly poll the poll status. If it sees that it's already
+//     voted, then it will gray out the buttons. If it sees that the last open
+//     poll has results, then it will simply use those.
+//     Sends a "vote" request. Server keeps IPs hashed against votes. May send
+//     success or failure.
+//
+
 sys.log("Starting up...");
 routes.addRoutes(
   {
