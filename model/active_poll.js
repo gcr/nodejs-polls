1ecd12abba9df5e8fdd50e22b8799c7b8eb85f80
Michael W <hotdog003@gmail.com>
2010-07-14 08:21:43 -0600

    Polls now time out after a while (5hr by defoult)
    If a poll isn't touched for 5hr, then it'll disappear.

--- model/active_poll.js
+++ model/active_poll.js
@@ -1,15 +1,37 @@
-// Contains a simple container for setting and getting the active poll.
+// Contains a simple container for setting and getting the active poll. Also
+// employs a simple timer for expiring a poll if nobody's touched it for a
+// while.
 //
+
+var sys = require('sys'),
+    POLL_TIMEOUT = 1000 * 60 * 60 * 5; // 5hr
 
 function Container() {
   this.item = null;
+  this.timer = null;
 }
+
+Container.prototype.clear = function() {
+  // TODO! Get some kind of log framework
+  sys.log("Clearing poll (inactivity)");
+  this.item = null;
+  clearTimeout(this.timer);
+  this.timer = null;
+};
 
 Container.prototype.set = function(newObj) {
   this.item = newObj;
+  this.poke();
+};
+
+Container.prototype.poke = function() {
+  var self = this;
+  clearTimeout(this.timer);
+  this.timer = setTimeout(function(){self.clear();}, POLL_TIMEOUT);
 };
 
 Container.prototype.get = function() {
+  this.poke();
   return this.item;
 };
 
@@ -18,7 +40,7 @@ Container.prototype.curryGet = function(func) {
   // function.
   var self = this;
   return function() {
-    var args=[self.item];
+    var args=[self.get()];
     for (var i=0,l=arguments.length; i<l; i++) {
         args.push(arguments[i]);
     }
