5fc2d648d37d025160c42e1d25d02bc503a2f98a
Michael W <hotdog003@gmail.com>
2010-07-13 22:30:34 -0600

    Lots of poll changes

--- static/js/student.js
+++ static/js/student.js
@@ -13,9 +13,13 @@ $(document).ready(function() {
   var POLL_FREQUENCY=1000,
       // Hacks, sorry
       currentPage = getCurrentPage(window.location.pathname.split("/")),
-      pollId=0;
+      pollId=0; // pollId is used to verify that the poll didn't change.
 
   if (currentPage=="poll" || currentPage=="results") {
+    // How do we get the poll ID? Why, in a hidden <div/> of course!
+    // Sorry, but the only other option is AJAX, which has threading issues
+    // because we need to ensure the poll won't change between the time when the
+    // user gets the page and the teacher changes the question.
     pollId = parseInt($("#pollId").text(),10);
   }
 
